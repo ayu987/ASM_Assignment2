b36d0af0bb5311f20bb5a38d11bd43326ce7ff2e
vikram2000b <vikram2000b@gmail.com>
2020-11-19 13:17:58 +0530

    m

--- main.R
+++ main.R
@@ -1,2 +1,9 @@
 source("data_transformation.R")
+source("training.R")
 transform("data.csv", "_final_data.csv")
+
+data = read.csv("_final_data.csv")
+print(data$Interevent_Count/100)
+model = model.fit(data$Interevent_Count, "weibull", "mle")
+plot(model)
+print(summary(model))
--- training.R
+++ training.R
@@ -1,1 +1,15 @@
+library(fitdistrplus)
+library(actuar)
+model.fit <- function(data, distribution, method){
+    if((distribution == "gamma" || distribution == "weibull") && method == "mle"){
+        model <- fitdist(data[data>0], distr = distribution, method = method, lower = c(0,0)) # start = list(scale = 1, shape = 1)
+        return(model)
+    }
 
+    if(distribution == "weibull"){
+        model <- fitdist(data[data>0], distr = distribution, method = method, lower = c(0,0), order=c(1,2),memp="momentfunc")
+    }
+
+    model <- fitdist(data[data>0], distr = distribution, method = method)
+    return(model)
+}
