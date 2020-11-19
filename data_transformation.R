1180dd6413f8cbae66cec5308f30048f4c4bfe9a
vikram2000b <vikram2000b@gmail.com>
2020-11-19 10:20:47 +0530

    Adding transformed data and completed transformation method

--- data_transformation.R
+++ data_transformation.R
@@ -1,23 +1,24 @@
-#import the csv file
-data <- read.csv("data.csv")
-print("Row CSV file looks like this: \n")
-print(str(data))
 
-# cleaning unncessary columns (Column X, X.1, X.2)
-print(" Data After Cleaning: ")
-data <- subset(data, select = -c(X, X.1, X.2))
-print(str(data))
+transform <- function(input_file, output_file){
+    #import the csv file
+    data <- read.csv(input_file)
 
-# Now creating a boolean vector which stores whether nth earthquake of dataframe is above 6 or not
-is_large <- (data$MAG >= 6)
-print(head(is_large))
+    # Now creating a new dataframe which stores the data as we require (in form of interevent time (Natural time))
+    final_data <- data.frame(DATE = character(), TIME = character(), LAT = double(), LON = double(), 
+        DEPTH = double(), MAG = double(), Interevent_Count = integer(), stringsAsFactors = FALSE)
 
-# Adding one column in dataframe which stores whether the earthquka is >= 6 or not
-data <- cbind(data, data.frame(is_large = is_large))
-print(head(data))
+    interevent_count = 0
+    for (row in 1:nrow(data)){
+        if(as.double(data[row, "MAG"]) < 6){
+            interevent_count = interevent_count + 1
+        }
+        else{
+            new_row <- list(data[row, "DATE"], data[row, "TIME"], as.double(data[row, "LAT"]), as.double(data[row, "LON"]), 
+                as.double(data[row, "DEPTH"]), as.double(data[row, "MAG"]), interevent_count)
+            final_data[nrow(final_data)+1, ] <- new_row
+            interevent_count = 0
+        }
+    }
 
-# Now creating a new dataframe which stores the data as we require (in form of interevent time (Natural time))
-final_data <- data.frame(DATE = character(), TIME = character(), LAT = double(), LON = double(), DEPTH = double(), DEPFIX = logical(), MAG = double(), Interevent_Count = integer())
-
-#final_data <- rbind(final_data, c(data[1,],0))
-print(head(final_data))
+    write.csv(final_data,output_file)
+}
--- main.R
+++ main.R
@@ -1,1 +1,2 @@
-
+source("data_transformation.R")
+transform("data.csv", "_final_data.csv")
