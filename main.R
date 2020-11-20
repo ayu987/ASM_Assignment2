8db24f7a6633a11fe5bedddc5f8ec25234497432
ayu987 <f20180258@pilani.bits-pilani.ac.in>
2020-11-20 19:09:42 +0530

    commented summary in main.R

--- main.R
+++ main.R
@@ -6,7 +6,7 @@ transform("data.csv", "_final_data.csv")
 data = read.csv("_final_data.csv")
 model = model.fit(data$Interevent_Count, "weibull", "mme")
 plot(model)
-summary(model)[2]/ model$method = type of method used for parameter estimatin (mle/mme).
-summary(model)[6] = log likelihood
-summary(model)[7] = AIC
-summary(model)[8] = BIC
+#summary(model)[2] = model$method # type of method used for parameter estimatin (mle/mme).
+#summary(model)[6] = log likelihood
+#summary(model)[7] = AIC
+#summary(model)[8] = BIC
