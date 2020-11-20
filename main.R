1cd670aad151790d5a11e026ed27191008058ba1
ayu987 <f20180258@pilani.bits-pilani.ac.in>
2020-11-20 21:31:36 +0530

    all distributions supported now except exp exp

--- main.R
+++ main.R
@@ -1,14 +1,16 @@
-library(distributions3)
 source("data_transformation.R")
 source("training.R")
 transform("data.csv", "_final_data.csv")
 
 data = read.csv("_final_data.csv")
-model = model.fit(data$Interevent_Count, "weibull", "mle")
-plot(model)
-#summary(model)[2] = model$method # type of method used for parameter estimatin (mle/mme).
-#summary(model)[6] = log likelihood
-#summary(model)[7] = AIC
-#summary(model)[8] = BIC
+model = model.fit(data$Interevent_Count, "weibull", "mle")  #weibull, gamma, exp, invgauss, lnorm, norm, 
+                                                          #invweibull, pareto, chisq, 
+#plot(model)
+# summary(model)[2] = model$method # type of method used for parameter estimatin (mle/mme).
+# summary(model)[6] = log likelihood
+# summary(model)[7] = AIC
+# summary(model)[8] = BIC
+
+#model$estimate
 
 ks_test(data$Interevent_Count, model)
--- training.R
+++ training.R
@@ -1,15 +1,27 @@
 library(fitdistrplus)
 library(actuar)
+library(distributions3)
+#library(VGAM)
+
+source("expweibull.R")
 
 model.fit <- function(data, distribution, method){
     if((distribution == "gamma" || distribution == "weibull") && method == "mle"){
         model <- fitdist(data[data>0], distr = distribution, method = method, lower = c(0,0)) # start = list(scale = 1, shape = 1)
         return(model)
     }
-
-    if(distribution == "weibull" && method == "mme"){
+    else if(distribution == "weibull" && method == "mme"){
     memp  <-  function(x, order) mean(x^order) 
     model<- fitdist(data[data>0], "weibull", method = "mme", order=c(1, 2), memp=memp, lower = c(0, 0))
+    }
+    else if(distribution == "invgauss"){
+        model <- fitdist(data[data>0], "invgauss", start = list(mean = 5, shape = 1))
+    }
+    else if(distribution == "expweibull"){
+        model <- fitdist(data[data>0], distr = distribution, start = list(kappa = 1, lambda = 1, alpha=1), method = method)
+    }
+    else if(distribution =='chisq'){
+        model <- fitdist(data[data>0], distr = distribution, start = list(ncp=1, df=1), lower=c(0, 0),  method = method)
     }
 
     else model <- fitdist(data[data>0], distr = distribution, method = method)
@@ -24,7 +36,28 @@ ks_test <- function(data, model){
     if(model$distname == "weibull"){
         return(ks.test(data, "pweibull", model$estimate["shape"], model$estimate["scale"]))
     }
-    #if(model$distname == "exp"){
-        
-    #}
+    if(model$distname == "exp"){
+        return(ks.test(data, "pexp", model$estimate["rate"]))
+    }
+    if(model$distname == "invgauss"){
+        return(ks.test(data, "pinvgauss", model$estimate["mean"], model$estimate["shape"]))
+    }
+    if(model$distname == "lnorm"){
+        return(ks.test(data, "plnorm", model$estimate["meanlog"], model$estimate["sdlog"]))
+    }
+    if(model$distname == "norm"){
+        return(ks.test(data, "pnorm", model$estimate["mean"], model$estimate["sd"]))
+    }
+    if(model$distname == "invweibull"){
+        return(ks.test(data, "pinvweibull", model$estimate["shape"], model$estimate["scale"]))
+    }
+    if(model$distname == "pareto"){
+        return(ks.test(data, "ppareto", model$estimate["shape"], model$estimate["scale"]))
+    }
+    if(model$distname == "expweibull"){
+        return(ks.test(data, "pexpweibull", model$estimate["lambda"], model$estimate["kappa"], model$estimate["alpha"]))
+    }
+    if(model$distname == "chisq"){
+        return(ks.test(data, "pchisq", model$estimate["ncp"], model$estimate["df"]))
+    }
 }
