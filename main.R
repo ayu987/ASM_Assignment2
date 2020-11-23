8242ea9b410e2dd2dc02ead46c0b00913986e77e
ayu987 <f20180258@pilani.bits-pilani.ac.in>
2020-11-23 10:41:27 +0530

    expexp is a special case of expweibull

--- main.R
+++ main.R
@@ -5,7 +5,7 @@ library(reliaR)
 transform("data.csv", "_final_data.csv")
 
 data = read.csv("_final_data.csv")
-model = model.fit(data$Interevent_Count, "", "mle")  #weibull, gamma, exp, invgauss, lnorm, norm, 
+model = model.fit(data$Interevent_Count, "expweibull", "mle")  #weibull, gamma, exp, invgauss, lnorm, norm, 
                                                           #invweibull, pareto, chisq, geom, expweibull
 #plot(model)
 # summary(model)[2] = model$method # type of method used for parameter estimatin (mle/mme).
@@ -17,7 +17,7 @@ print(ks_test(data$Interevent_Count, model))
 
 
 #code for exponentiated exponential
-model <- vglm(Interevent_Count ~ 1, fam = expexpff, data = data[which(data$Interevent_Count>0),], trace = TRUE, maxit = 99)
-coef(fit, matrix = TRUE)
-Coef(fit)
-ks.gen.exp(as.integer(data[which(data$Interevent_Count>0),]$Interevent_Count), 0.742549412, 0.007017575, alternative = "two.sided")
+# model <- vglm(Interevent_Count ~ 1, fam = expexpff, data = data[which(data$Interevent_Count>0),], trace = TRUE, maxit = 99)
+# coef(fit, matrix = TRUE)
+# Coef(fit)
+# ks.gen.exp(as.integer(data[which(data$Interevent_Count>0),]$Interevent_Count), 0.742549412, 0.007017575, alternative = "two.sided")
